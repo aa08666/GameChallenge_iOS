afba4541cbc5a9a588688f5fdab4d21cf64cc829
BoCheng <jokerlin08666@gmail.com>
2018-12-20 10:27:16 +0800

    Ep11

--- GameChallenge_iOS/Controller/SignUpViewController.swift
+++ GameChallenge_iOS/Controller/SignUpViewController.swift
@@ -43,60 +43,128 @@ class SignUpViewController: UIViewController {
         save()
         print("123")
         
-        //解析JSON，從95行的completeion(data)中的 data 傳入
-        getData(from: "http://c86108bd.ngrok.io/api/register") { (data) in
-//            print("Call API")
-            
-            let decoder = JSONDecoder()
-            if let RegisterBodyData = try? decoder.decode(RegisterBodyData.self, from: data) {
-                //下面搞混了，
-                print(RegisterBodyData)
-//                print(RegisterBodyData.name)
-//                print(RegisterBodyData.email)
-//                print(RegisterBodyData.password)
-//                print(RegisterBodyData.password_confirmation)
-            }
-            if let RegisterSuccessResponse = try? decoder.decode(RegisterSuccessResponse.self, from: data) {
-                print(RegisterSuccessResponse)
-//                print(RegisterSuccessResponse.name)
-//                print(RegisterSuccessResponse.result)
-//                print(RegisterSuccessResponse.coin)
-//                print(RegisterSuccessResponse.api_token)
-               
-            }
-            
-        }
-        
-    }
-    //轉成data
-    func getData(from urlString: String, completeion: @escaping (Data) -> Void) {
-        // 判斷 urlString 是否能被轉成 Url，若無法則 return，不繼續後面的操作。
-        guard let url = URL(string: urlString) else { return }
-        let json = [
-            "name": nameTextField,
-            "email": emailTextField,
-            "password": passwordTextField,
-            "password_confirmation": passwordConTextField
-        ]
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        let data = try? JSONSerialization.data(withJSONObject: json, options: [])
-        request.httpBody = data
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        // 使用 URLSession.shared.data(with: url) 來獲取網址中的數據。
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            // 如果 error 存在，印出錯誤訊息後，不接續後面操作。
-            if error != nil {
-                print(error!.localizedDescription)
+        func makePostCall() {
+            let todosEndpoint: String = "http://c86108bd.ngrok.io/api/register"
+            guard let todosURL = URL(string: todosEndpoint) else {
+                print("Error: cannot create URL")
                 return
             }
-            // 檢查 data 是否存在，若是存在透過 compeletion 這個逃逸閉包（escaping clourse）將 data 傳遞出去。若是不存在則 return。
-            guard let data = data else { return }
+            var todosUrlRequest = URLRequest(url: todosURL)
+            todosUrlRequest.httpMethod = "POST"
+            let newTodo: [String: Any] = ["title": "My First todo", "completed": false, "userId": 1]
+            let jsonTodo: Data
+            do {
+                jsonTodo = try JSONSerialization.data(withJSONObject: newTodo, options: [])
+                todosUrlRequest.httpBody = jsonTodo
+            } catch {
+                print("Error: cannot create JSON from todo")
+                return
+            }
             
-            completeion(data)
+            let session = URLSession.shared
             
-            }.resume() // 別忘了加上 resume() 才會開始運作。
-    }
+            let task = session.dataTask(with: todosUrlRequest) {
+                (data, response, error) in
+                guard error == nil else {
+                    print("error calling POST on /todos/1")
+                    print(error!)
+                    return
+                }
+                guard let responseData = data else {
+                    print("Error: did not receive data")
+                    return
+                }
+                
+                // parse the result as JSON, since that's what the API provides
+                do {
+                    guard let receivedTodo = try JSONSerialization.jsonObject(with: responseData,
+                                                                              options: []) as? [String: Any] else {
+                                                                                print("Could not get JSON from responseData as dictionary")
+                                                                                return
+                    }
+                    print("The todo is: " + receivedTodo.description)
+                    
+                    guard let todoID = receivedTodo["id"] as? Int else {
+                        print("Could not get todoID as int from JSON")
+                        return
+                    }
+                    print("The ID is: \(todoID)")
+                } catch  {
+                    print("error parsing response from POST on /todos")
+                    return
+                }
+            }
+            task.resume()
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        //解析JSON，從95行的completeion(data)中的 data 傳入
+//        getData(from: "http://c86108bd.ngrok.io/api/register") { (data) in
+////            print("Call API")
+//
+//            let decoder = JSONDecoder()
+//            if let RegisterBodyData = try? decoder.decode(RegisterBodyData.self, from: data) {
+//                //下面搞混了，
+//                print(RegisterBodyData)
+////                print(RegisterBodyData.name)
+////                print(RegisterBodyData.email)
+////                print(RegisterBodyData.password)
+////                print(RegisterBodyData.password_confirmation)
+//            }
+//            if let RegisterSuccessResponse = try? decoder.decode(RegisterSuccessResponse.self, from: data) {
+//                print(RegisterSuccessResponse)
+////                print(RegisterSuccessResponse.name)
+////                print(RegisterSuccessResponse.result)
+////                print(RegisterSuccessResponse.coin)
+////                print(RegisterSuccessResponse.api_token)
+//
+//            }
+//
+//        }
+//
+//    }
+//    //轉成data
+//    func getData(from urlString: String, completeion: @escaping (Data) -> Void) {
+//        // 判斷 urlString 是否能被轉成 Url，若無法則 return，不繼續後面的操作。
+//        guard let url = URL(string: urlString) else { return }
+//        let json = [
+//            "name": nameTextField,
+//            "email": emailTextField,
+//            "password": passwordTextField,
+//            "password_confirmation": passwordConTextField
+//        ]
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        let data = try? JSONSerialization.data(withJSONObject: json, options: [])
+//        request.httpBody = data
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//
+//        // 使用 URLSession.shared.data(with: url) 來獲取網址中的數據。
+//        URLSession.shared.dataTask(with: request) { (data, response, error) in
+//
+//            // 如果 error 存在，印出錯誤訊息後，不接續後面操作。
+//            if error != nil {
+//                print(error!.localizedDescription)
+//                return
+//            }
+//            // 檢查 data 是否存在，若是存在透過 compeletion 這個逃逸閉包（escaping clourse）將 data 傳遞出去。若是不存在則 return。
+//            guard let data = data else { return }
+//
+//            completeion(data)
+//
+//            }.resume() // 別忘了加上 resume() 才會開始運作。
+//    }
+}
+
 }
